1de9f1b5a7e516110862bbd9623fd1cde6eb7de6
Sharkri <sharkdude955@gmail.com>
2022-09-20 19:14:35 -0500

    add wind, pressure and humidity

--- src/api.js
+++ src/api.js
@@ -1,8 +1,6 @@
-// api key 776fcd1eecdd73f0d7b531362234442a
-
 async function getWeatherInfo(location) {
   const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=776fcd1eecdd73f0d7b531362234442a`
+    `https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=776fcd1eecdd73f0d7b531362234442a&units=imperial`
   );
 
   const data = await response.json();
--- src/index.js
+++ src/index.js
@@ -5,8 +5,10 @@ const search = document.querySelector("#search-input");
 const temperature = document.querySelector(".temperature");
 const location = document.querySelector(".location-info");
 const time = document.querySelector(".current-time");
-const kelvinToFahrenheit = (k) => ((k - 273.15) * 9) / 5 + 32;
-
+const weather = document.querySelector(".weather");
+const humidity = document.querySelector(".humidity");
+const windSpeed = document.querySelector(".wind-speed");
+const pressure = document.querySelector(".pressure");
 function getTime(timezone) {
   const localTime = new Date().getTime();
   const localOffset = new Date().getTimezoneOffset() * 60000;
@@ -16,16 +18,27 @@ function getTime(timezone) {
   return format(cityDate, "EEEE, h:mm a");
 }
 
+function setWeatherInfo(data) {
+  temperature.textContent = `${data.main.temp.toFixed(0)}°`;
+  time.textContent = getTime(data.timezone);
+  location.textContent = data.name;
+  weather.textContent = data.weather[0].description;
+  humidity.textContent = `${data.main.humidity}%`;
+  humidity.title = `Humidity: ${data.main.humidity}`;
+  windSpeed.textContent = `${data.wind.speed} mph`;
+  pressure.textContent = `${data.main.pressure} hpa`;
+}
+
 search.addEventListener("keydown", (e) => {
-  if (!e.key === "Enter") return;
-  // if key is enter show weather stuff
+  if (e.key !== "Enter") return;
+
   getWeatherInfo(search.value).then((data) => {
-    // Clear search
+    // Clear and unfocus search input
     search.value = "";
     search.blur();
-    const convertedTemp = kelvinToFahrenheit(data.main.temp);
-    time.textContent = getTime(data.timezone);
-    temperature.textContent = `${convertedTemp.toFixed(0)}°`;
-    location.textContent = data.name;
+    setWeatherInfo(data);
   });
 });
+
+// Initial Load
+getWeatherInfo("London").then(setWeatherInfo);
